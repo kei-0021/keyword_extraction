639bb1431dc90768df9acd74dd029985b2f030c3
kei-0021 <quita02@gmail.com>
2025-05-05 01:21:43 +0000

    issue #1 ユーザー辞書とストップワードきちんと動作していることを確認

--- source/word_analyser.py
+++ source/word_analyser.py
@@ -13,18 +13,20 @@ def analyse_word(text: str, custom_dict_path: str, stop_words_path: str) -> Coun
     stop_words = load_stop_words(stop_words_path)
     
     # MeCabインスタンスを作成
-    tagger = MeCab.Tagger(f"-d /var/lib/mecab/dic/ipadic-utf8 -u {custom_dict_path}")
-    
+    tagger = MeCab.Tagger(f"-r /etc/mecabrc -d /var/lib/mecab/dic/ipadic-utf8 -u {custom_dict_path}")
+
     # 形態素解析を行い、結果を取得
     node: MeCab.Node = tagger.parseToNode(text)
     
     # 名詞のみを抽出
     noun_list = []
     while node:
-        if "名詞" in node.feature:
+        features = node.feature.split(",")
+        if features[0] == "名詞":
             if node.surface not in stop_words and node.surface != "":
                 noun_list.append(node.surface)
         node = node.next
+
     
     # 名詞のみをカウントして返す
     return Counter(noun_list)

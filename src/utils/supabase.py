8fa8c747311ea4529592a60528932a65d38bf7e8
kei-0021 <quita02@gmail.com>
2025-07-01 09:39:27 +0000

    issue #3 ローカルで簡易サーバーを立ててブラウザ上で確認できるようにしました

--- src/utils/supabase.py
+++ src/utils/supabase.py
@@ -1,12 +1,14 @@
 import os
 
 import streamlit as st
+from dotenv import load_dotenv
 from supabase import Client, create_client
 
 
 def get_supabase_client() -> Client:
     """Supabaseクライアントを生成または取得する関数."""
     if "supabase" not in st.session_state:
+        load_dotenv("config/.env")
         url = os.getenv("SUPABASE_URL")
         key = os.getenv("SUPABASE_KEY")
         st.session_state.supabase = create_client(url, key)
